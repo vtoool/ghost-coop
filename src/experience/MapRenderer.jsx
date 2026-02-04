2fb0c0dfa8f204c9c6afab9e7695eaa80a69e3b5
Victor Bujor <victor@cabin-story.com>
2026-02-04 02:24:58 +0200

    Apply texture mapping: platformer atlas to floor, graveyard atlas to props

--- src/experience/MapRenderer.jsx
+++ src/experience/MapRenderer.jsx
@@ -4,46 +4,17 @@ import { RigidBody, CuboidCollider } from '@react-three/rapier'
 import * as THREE from 'three'
 import { level1, mapLegend } from './LevelMap'
 
-const GRID_SIZE = 2
-const WIDTH = level1[0].length
-const HEIGHT = level1.length
-const OFFSET_X = (WIDTH * GRID_SIZE) / 2
-const OFFSET_Z = (HEIGHT * GRID_SIZE) / 2
-
-function MapTile({ name, position, texture }) {
-  const { scene } = useGLTF(`/models/environment/${name}.glb`)
-  
-  const clone = useMemo(() => {
-    const c = scene.clone()
-    c.traverse((child) => {
-      if (child.isMesh) {
-        child.material = child.material.clone()
-        child.material.map = texture
-        child.castShadow = true
-        child.receiveShadow = true
-      }
-    })
-    return c
-  }, [scene, texture])
-  
-  return (
-    <RigidBody type="fixed" colliders="cuboid" position={position}>
-      <primitive object={clone} />
-    </RigidBody>
-  )
-}
-
-export default function MapRenderer() {
+export function MapRenderer() {
   const platformerTx = useTexture('/models/environment/Textures/colormap_platformer.png')
   platformerTx.colorSpace = THREE.SRGBColorSpace
   platformerTx.flipY = false
-  
+
   const graveyardTx = useTexture('/models/environment/Textures/colormap_graveyard.png')
   graveyardTx.colorSpace = THREE.SRGBColorSpace
   graveyardTx.flipY = false
-  
+
   const { scene: floorScene } = useGLTF('/models/environment/block-grass.glb')
-  
+
   const floorClone = useMemo(() => {
     const clone = floorScene.clone()
     clone.traverse((child) => {
@@ -56,25 +27,31 @@ export default function MapRenderer() {
     })
     return clone
   }, [floorScene, platformerTx])
-  
+
+  const gridSize = 2
+  const width = level1[0].length
+  const height = level1.length
+  const offsetX = width * gridSize / 2
+  const offsetZ = height * gridSize / 2
+
   const floorTiles = []
-  for (let z = 0; z < HEIGHT; z++) {
-    for (let x = 0; x < WIDTH; x++) {
+  for (let z = 0; z < height; z++) {
+    for (let x = 0; x < width; x++) {
       floorTiles.push(
         <primitive 
           key={`floor-${x}-${z}`}
           object={floorClone.clone()}
-          position={[x * GRID_SIZE - OFFSET_X, -1, z * GRID_SIZE - OFFSET_Z]}
+          position={[x * gridSize - offsetX, -1, z * gridSize - offsetZ]}
         />
       )
     }
   }
-  
+
   const props = []
   let index = 0
-  for (let z = 0; z < HEIGHT; z++) {
+  for (let z = 0; z < height; z++) {
     const row = level1[z]
-    for (let x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < width; x++) {
       const char = row[x]
       if (char === '.' || char === ' ') continue
       
@@ -82,9 +59,9 @@ export default function MapRenderer() {
       if (!name) continue
       
       const position = [
-        x * GRID_SIZE - OFFSET_X,
+        x * gridSize - offsetX,
         0,
-        z * GRID_SIZE - OFFSET_Z
+        z * gridSize - offsetZ
       ]
       
       props.push(
@@ -97,7 +74,7 @@ export default function MapRenderer() {
       )
     }
   }
-  
+
   return (
     <group>
       <group>{floorTiles}</group>
@@ -106,5 +83,27 @@ export default function MapRenderer() {
         <CuboidCollider args={[50, 1, 50]} />
       </RigidBody>
     </group>
+  )
+}
+
+function MapTile({ name, position, texture }) {
+  const { scene } = useGLTF(`/models/environment/${name}.glb`)
+  const clone = useMemo(() => {
+    const c = scene.clone()
+    c.traverse((child) => {
+      if (child.isMesh) {
+        child.material = child.material.clone()
+        child.material.map = texture
+        child.castShadow = true
+        child.receiveShadow = true
+      }
+    })
+    return c
+  }, [scene, texture])
+
+  return (
+    <RigidBody type="fixed" colliders="hull" position={position}>
+      <primitive object={clone} />
+    </RigidBody>
   )
 }
