9af427e5137a9b9180dab843c4ec1b1d3fb2222e
Victor Bujor <victor@cabin-story.com>
2026-02-04 13:48:06 +0200

    refactor: simplify floor to single giant mesh, remove auto-tiling

--- src/experience/MapRenderer.jsx
+++ src/experience/MapRenderer.jsx
@@ -5,104 +5,33 @@ import * as THREE from 'three'
 import { level1, mapLegend } from './LevelMap'
 
 export function MapRenderer() {
-  const platformerTx = useTexture('/models/environment/Textures/colormap_platformer.png')
-  platformerTx.colorSpace = THREE.SRGBColorSpace
-  platformerTx.flipY = false
-
   const graveyardTx = useTexture('/models/environment/Textures/colormap_graveyard.png')
   graveyardTx.colorSpace = THREE.SRGBColorSpace
   graveyardTx.flipY = false
 
-  const { scene: roundedScene } = useGLTF('/models/environment/block-grass.glb')
-  const { scene: squareScene } = useGLTF('/models/environment/block-grass-square.glb')
-
-  const roundedClone = useMemo(() => {
-    const clone = roundedScene.clone()
-    clone.traverse((child) => {
-      if (child.isMesh) {
-        child.material = child.material.clone()
-        child.material.map = platformerTx
-        child.castShadow = true
-        child.receiveShadow = true
-      }
-    })
-    return clone
-  }, [roundedScene, platformerTx])
-
-  const squareClone = useMemo(() => {
-    const group = new THREE.Group()
-
-    const dirtGeo = new THREE.BoxGeometry(2.3, 1, 2.3)
-    const dirtMat = new THREE.MeshStandardMaterial({
-      color: '#6d4e3d',
-      roughness: 1,
-    })
-    const dirtMesh = new THREE.Mesh(dirtGeo, dirtMat)
-    dirtMesh.position.set(0, 0.49, 0)
-    dirtMesh.castShadow = true
-    dirtMesh.receiveShadow = true
-    group.add(dirtMesh)
-
-    const grassMesh = squareScene.clone()
-    const box = new THREE.Box3().setFromObject(grassMesh)
-    const size = new THREE.Vector3()
-    box.getSize(size)
-
-    const targetSize = 2.3
-    const scaleX = size.x > 0 ? targetSize / size.x : 2.3
-    const scaleZ = size.z > 0 ? targetSize / size.z : 2.3
-
-    grassMesh.scale.set(scaleX, 1, scaleZ)
-
-    grassMesh.traverse((child) => {
-      if (child.isMesh) {
-        child.material = new THREE.MeshStandardMaterial({
-          color: '#6BAE45',
-          roughness: 0.9,
-        })
-        child.castShadow = true
-        child.receiveShadow = true
-      }
-    })
-
-    grassMesh.position.set(0, 1.0, 0)
-
-    group.add(grassMesh)
-
-    return group
-  }, [squareScene])
-
   const gridSize = 2
   const width = level1[0].length
   const height = level1.length
-  const offsetX = width * gridSize / 2
-  const offsetZ = height * gridSize / 2
 
-  const floorTiles = []
-  for (let z = 0; z < height; z++) {
-    for (let x = 0; x < width; x++) {
-      const isEdge = x === 0 || x === width - 1 || z === 0 || z === height - 1
-      const model = isEdge ? roundedClone : squareClone
+  const mapWidth = width * gridSize
+  const mapHeight = height * gridSize
 
-      floorTiles.push(
-        <primitive
-          key={`floor-${x}-${z}`}
-          object={model.clone()}
-          position={[x * gridSize - offsetX, -1, z * gridSize - offsetZ]}
-        />
-      )
-    }
-  }
+  const offsetX = mapWidth / 2
+  const offsetZ = mapHeight / 2
 
   const props = level1.flatMap((row, z) =>
     row.split('').map((char, x) => {
       const name = mapLegend[char]
       if (!name) return null
+
+      const posX = (x * gridSize) - offsetX + (gridSize / 2)
+      const posZ = (z * gridSize) - offsetZ + (gridSize / 2)
+
       return (
         <MapTile
           key={`prop-${x}-${z}`}
           name={name}
-          position={[x * gridSize - offsetX, 0, z * gridSize - offsetZ]}
+          position={[posX, 0, posZ]}
           texture={graveyardTx}
         />
       )
@@ -111,11 +40,16 @@ export function MapRenderer() {
 
   return (
     <group>
-      <group>{floorTiles}</group>
-      <group>{props}</group>
-      <RigidBody type="fixed" position={[0, -1, 0]}>
-        <CuboidCollider args={[50, 1, 50]} />
+      <mesh position={[0, -0.5, 0]} receiveShadow>
+        <boxGeometry args={[mapWidth, 1, mapHeight]} />
+        <meshStandardMaterial color="#6BAE45" roughness={0.8} />
+      </mesh>
+
+      <RigidBody type="fixed" position={[0, -0.5, 0]} colliders={false}>
+        <CuboidCollider args={[mapWidth / 2, 0.5, mapHeight / 2]} />
       </RigidBody>
+
+      <group>{props}</group>
     </group>
   )
 }
