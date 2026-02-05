aa6e98928011eca3ed4c84281149d335d2cc920f
Victor Bujor <victor@cabin-story.com>
2026-02-05 18:07:24 +0200

    feat: revert road to single tile instancing (scale=1, randomRotation, visual-only)

--- src/experience/MapRenderer.tsx
+++ src/experience/MapRenderer.tsx
@@ -117,11 +117,7 @@ function useMapParser() {
             positions.iron_fence.push(pos)
           }
         } else if (modelName === 'road') {
-          for (let i = 0; i < 10; i++) {
-            const offsetX = (Math.random() - 0.5) * 1.6
-            const offsetZ = (Math.random() - 0.5) * 1.6
-            positions.road.push([pos[0] + offsetX, 0.02, pos[2] + offsetZ])
-          }
+          positions.road.push([pos[0], 0.02, pos[2]])
         } else {
           if (positions[modelName as keyof ModelPositions]) {
             positions[modelName as keyof ModelPositions].push(pos)
@@ -245,8 +241,8 @@ export function MapRenderer() {
       <Instancer
         model="road"
         positions={positions.road}
-        scale={[0.6, 0.15, 0.6]}
-        randomRotation
+        scale={1}
+        randomRotation={true}
         collider={undefined}
       />
 
